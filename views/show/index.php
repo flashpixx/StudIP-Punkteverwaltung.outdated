e935f7fc4ab43932c2ebea3dd8a02ab36a9229d9
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-18 14:55:00 +0200

    matrikelnr

--- views/show/index.php
+++ views/show/index.php
@@ -1,7 +1,12 @@
 <?php
 
-    echo "test : ".$answer;
+    require_once("../../matrikelnummer/factory.class.php");
+
+    $loMatrikelNr = MatrikelNummerFactory::get();
+
+    echo "test : ".$answer."<br/>";
     echo "user id : ".$currentuser->auth["uid"];
+    echo "matrikelnr : ".$loMatrikelNr.get($currentuser->auth["uid"]);
     echo "<pre>".print_r($currentuser, true)."</pre>";
 
 ?>
