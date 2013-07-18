a905f90e01d2c4560aba0eda8f6d6739b4e316e4
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-18 16:08:11 +0200

    matrikel

--- views/show/index.php
+++ views/show/index.php
@@ -2,11 +2,11 @@
 
     require_once( dirname(dirname(__DIR__)) . "/matrikelnummer/factory.class.php");
 
-    //$loMatrikelNr = MatrikelNummerFactory::get();
+    $loMatrikelNr = MatrikelNummerFactory::get();
 
     echo "test : ".$answer."<br/>";
     echo "user id : ".$userseminar->id;
-    //echo "matrikelnr : ".$loMatrikelNr->get($userseminar->id);
+    echo "matrikelnr : ".$loMatrikelNr->get($userseminar->id);
     echo "<pre>".print_r($userseminar, true)."</pre>";
 
 ?>
