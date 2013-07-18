8958633041052af75860379cb9676495e49168ae
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-18 17:48:46 +0200

    sql tables

---
+++ sql/install.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS `ppv_seminar` (
+  `id` varchar(32) NOT NULL,
+  `bestehenprozent` decimal(3,2) unsigned NOT NULL,
+  `uebungnichtbestanden` int(10) unsigned NOT NULL,
+  `bemerkung` longtext,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ppv_uebung` (
+  `seminar` varchar(32) NOT NULL,
+  `id` varchar(32) COLLATE utf8_bin NOT NULL,
+  `bestehenprozent` decimal(3,2) unsigned NOT NULL,
+  `maxpunkte` int(10) unsigned NOT NULL,
+  `bemerkung` longtext,
+  PRIMARY KEY (`seminar`,`id`),
+  KEY `seminar` (`seminar`)
+);
+
+CREATE TABLE IF NOT EXISTS `ppv_uebungstudent` (
+  `uebung` varchar(32) NOT NULL,
+  `student` varchar(32) NOT NULL,
+  `erreichtepunkte` decimal(3,2) unsigned NOT NULL,
+  `zusatzpunkte` decimal(3,2) unsigned NOT NULL,
+  `bemerkung` longtext,
+  PRIMARY KEY (`uebung`,`student`)
+);
---
+++ sql/uninstall.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS `ppv_seminar`;
+DROP TABLE IF EXISTS `ppv_uebung`;
+DROP TABLE IF EXISTS `ppv_uebungstudent`;
