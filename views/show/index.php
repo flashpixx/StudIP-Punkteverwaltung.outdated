18bc234a714c3634c32e96b4df5bcd489f9293e6
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-18 15:01:19 +0200

    userobject

--- views/show/index.php
+++ views/show/index.php
@@ -2,11 +2,11 @@
 
     require_once("../../matrikelnummer/factory.class.php");
 
-    $loMatrikelNr = MatrikelNummerFactory::get();
+    //$loMatrikelNr = MatrikelNummerFactory::get();
 
     echo "test : ".$answer."<br/>";
-    echo "user id : ".$currentuser->auth["uid"];
-    echo "matrikelnr : ".$loMatrikelNr->get($currentuser->auth["uid"]);
-    echo "<pre>".print_r($currentuser, true)."</pre>";
+    echo "user id : ".$userseminar->id;
+    //echo "matrikelnr : ".$loMatrikelNr->get($userseminar->id);
+    echo "<pre>".print_r($userseminar, true)."</pre>";
 
 ?>
