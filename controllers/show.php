4664a192fb906f26123471692e9176e90d92d298
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-18 14:43:01 +0200

    userobjekt

--- controllers/show.php
+++ controllers/show.php
@@ -6,6 +6,7 @@
         function before_filter( &$action, &$args )
         {
             $this->set_layout($GLOBALS["template_factory"]->open("layouts/base_without_infobox"));
+            $this->currentuser = $GLOBALS["user"];
             // PageLayout::setTitle("");
         }
 
--- views/show/index.php
+++ views/show/index.php
@@ -1,5 +1,6 @@
 <?php
 
     echo "test".$answer;
+    echo "<pre>".print_r($user, true)."</pre>";
 
 ?>
