8e74cae50382eeb0cfa6880fdfcc64dc5fbc7ae5
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-19 08:31:49 +0200

    table definition

--- sql/install.sql
+++ sql/install.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `ppv_seminar` (
 
 CREATE TABLE IF NOT EXISTS `ppv_uebung` (
   `seminar` varchar(32) NOT NULL,
-  `id` varchar(32) COLLATE utf8_bin NOT NULL,
+  `id` varchar(32) NOT NULL,
   `bestehenprozent` decimal(3,2) unsigned NOT NULL,
   `maxpunkte` int(10) unsigned NOT NULL,
   `bemerkung` longtext,
@@ -23,4 +23,17 @@ CREATE TABLE IF NOT EXISTS `ppv_uebungstudent` (
   `zusatzpunkte` decimal(3,2) unsigned NOT NULL,
   `bemerkung` longtext,
   PRIMARY KEY (`uebung`,`student`)
+);
+
+CREATE TABLE IF NOT EXISTS `ppv_uebungstudentlog` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uebung` varchar(32) NOT NULL,
+  `student` varchar(32) NOT NULL,
+  `korrektor` varchar(32) NOT NULL,
+  `erreichtepunkte` decimal(3,2) unsigned NOT NULL,
+  `zusatzpunkte` decimal(3,2) unsigned NOT NULL,
+  `bemerkung` longtext,
+  PRIMARY KEY (`id`),
+  KEY `uebung` (`uebung`,`student`),
+  KEY `korrektor` (`korrektor`)
 );
--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS `ppv_seminar`;
 DROP TABLE IF EXISTS `ppv_uebung`;
 DROP TABLE IF EXISTS `ppv_uebungstudent`;
+DROP TABLE IF EXISTS `ppv_uebungstudentlog`;
