1cd3ddddbef36bb4e2ef32247655c686441a95d3
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-25 15:33:06 +0200

    catch exception

--- views/admin/index.php
+++ views/admin/index.php
@@ -30,36 +30,42 @@
     require_once(dirname(dirname(__DIR__)) . "/sys/veranstaltung/veranstaltung.class.php");
 
 
-    $loVeranstaltung = isset($flash["veranstaltung"]) ? $flash["veranstaltung"] : null;
-    Tools::showMessage($flash["message"]);
+    try {
 
-    
-    if (!$loVeranstaltung)
-        echo "<a href=\"".$controller->url_for("admin/create")."\">"._("Für diese Veranstaltung die Punkteverwaltung aktivieren")."</a>";
-    else {
+        $loVeranstaltung = isset($flash["veranstaltung"]) ? $flash["veranstaltung"] : null;
+        Tools::showMessage($flash["message"]);
 
-        echo "<form method=\"post\" action=\"".$controller->url_for("admin/update")."\">\n";
-        CSRFProtection::tokenTag();
+        
+        if (!$loVeranstaltung)
+            echo "<a href=\"".$controller->url_for("admin/create")."\">"._("Für diese Veranstaltung die Punkteverwaltung aktivieren")."</a>";
+        else {
 
-        echo "<div class=\"steel1\">\n";
+            echo "<form method=\"post\" action=\"".$controller->url_for("admin/update")."\">\n";
+            CSRFProtection::tokenTag();
 
-        echo "<p>"._("Mit diesen Einstellungen können globale Optionen gesetzt werden, um den Übungsbetrieb zu verwalten. Hierzu zählt einmal die Angabe in Prozent (über die Summe aller Punkte aller Übungen), die für das bestehen der Veranstaltung notwendig sind. Mit der zweiten Option kann eine Anzahl an Übungen festgelegt werden, die unterhalb der individuell für jede Übung definierte Bestehensgrenze liegen dürfen, um noch zu einem Bestehen der Veranstaltung zu führen z.B. wenn eine Übung ausgelassen / nicht abgegeben werden darf, wäre der Wert 1 einzutragen. Die Bemerkung ist optinal und kann nur von Benutzern mit Dozenten oder Tutorrechten gesehen werden.")."<hr width=\"100%\"/></p>";
+            echo "<div class=\"steel1\">\n";
 
-        echo "<table width=\"100%\">\n";
+            echo "<p>"._("Mit diesen Einstellungen können globale Optionen gesetzt werden, um den Übungsbetrieb zu verwalten. Hierzu zählt einmal die Angabe in Prozent (über die Summe aller Punkte aller Übungen), die für das bestehen der Veranstaltung notwendig sind. Mit der zweiten Option kann eine Anzahl an Übungen festgelegt werden, die unterhalb der individuell für jede Übung definierte Bestehensgrenze liegen dürfen, um noch zu einem Bestehen der Veranstaltung zu führen z.B. wenn eine Übung ausgelassen / nicht abgegeben werden darf, wäre der Wert 1 einzutragen. Die Bemerkung ist optinal und kann nur von Benutzern mit Dozenten oder Tutorrechten gesehen werden.")."<hr width=\"100%\"/></p>";
 
-        echo "<tr><td width=\"50%\"><label for=\"bestandenprozent\">"._("Prozentzahl über die Summe aller Punkte, damit die Veranstaltung als bestanden gilt")."</label></td>";
-        echo "<td><input type=\"text\" id=\"bestandenprozent\" name=\"bestandenprozent\" value=\"".$loVeranstaltung->bestandenProzent()."\" size=\"35\"/></td></tr>\n";
+            echo "<table width=\"100%\">\n";
 
-        echo "<tr><td><label for=\"allow_nichtbestanden\">"._("Anzahl an nicht bestandenen Übungen, um die Veranstaltung trotzdem bei erreichen der Punkte als bestanden zu werten")."</label></td>";
-        echo "<td><input type=\"text\" id=\"allow_nichtbestanden\" name=\"allow_nichtbestanden\" value=\"".$loVeranstaltung->allowNichtBestanden()."\" size=\"35\"/></td></tr>\n";
+            echo "<tr><td width=\"50%\"><label for=\"bestandenprozent\">"._("Prozentzahl über die Summe aller Punkte, damit die Veranstaltung als bestanden gilt")."</label></td>";
+            echo "<td><input type=\"text\" id=\"bestandenprozent\" name=\"bestandenprozent\" value=\"".$loVeranstaltung->bestandenProzent()."\" size=\"35\"/></td></tr>\n";
 
-        echo "<tr><td><label for=\"bemerkung\">"._("Bemerkung")."</label></td>";
-        echo "<td><textarea id=\"bemerkung\" name=\"bemerkung\" cols=\"37\" rows=\"5\">".$loVeranstaltung->bemerkung()."</textarea></td></tr>\n";
+            echo "<tr><td><label for=\"allow_nichtbestanden\">"._("Anzahl an nicht bestandenen Übungen, um die Veranstaltung trotzdem bei erreichen der Punkte als bestanden zu werten")."</label></td>";
+            echo "<td><input type=\"text\" id=\"allow_nichtbestanden\" name=\"allow_nichtbestanden\" value=\"".$loVeranstaltung->allowNichtBestanden()."\" size=\"35\"/></td></tr>\n";
 
-        echo "</table>\n";
-        echo "</div>\n";
-        echo "<p><input type=\"submit\" name=\"submitted\" value=\""._("Angaben übernehmen")."\"/></p>";
-        echo "</form>";
+            echo "<tr><td><label for=\"bemerkung\">"._("Bemerkung")."</label></td>";
+            echo "<td><textarea id=\"bemerkung\" name=\"bemerkung\" cols=\"37\" rows=\"5\">".$loVeranstaltung->bemerkung()."</textarea></td></tr>\n";
+
+            echo "</table>\n";
+            echo "</div>\n";
+            echo "<p><input type=\"submit\" name=\"submitted\" value=\""._("Angaben übernehmen")."\"/></p>";
+            echo "</form>";
+        }
+        
+    } catch (Exception $e) {
+        Tools::showMessage( Tools::createMessage("error", $e->getMessage()) );
     }
 
 ?>
