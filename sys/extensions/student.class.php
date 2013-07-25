3cd041790c0e5b224aca4250f8259a710f5d539a
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-25 17:54:52 +0200

    add studentenclass

---
+++ sys/extensions/student.class.php
@@ -0,0 +1,100 @@
+<?php
+
+   /**
+    @cond
+    ############################################################################
+    # GPL License                                                              #
+    #                                                                          #
+    # This file is part of the StudIP-Punkteverwaltung.                        #
+    # Copyright (c) 2013, Philipp Kraus, <philipp.kraus@tu-clausthal.de>       #
+    # This program is free software: you can redistribute it and/or modify     #
+    # it under the terms of the GNU General Public License as                  #
+    # published by the Free Software Foundation, either version 3 of the       #
+    # License, or (at your option) any later version.                          #
+    #                                                                          #
+    # This program is distributed in the hope that it will be useful,          #
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+    # GNU General Public License for more details.                             #
+    #                                                                          #
+    # You should have received a copy of the GNU General Public License        #
+    # along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+    ############################################################################
+    @endcond
+    **/
+
+
+
+    require_once("matrikelnummer/factory.class.php");
+
+
+    /** Klasse um einen Studenten vollständig abzubilden **/
+    class Student
+    {
+        private $mcID = null;
+
+        
+
+        function __construct( $px )
+        {
+            if ($px instanceof $this)
+                $this->mcID = $px->id();
+            elseif (is_string($px))
+            {
+
+            }
+            else
+                throw new Exception("Benutzer nicht gefunden");
+        }
+
+
+        /** liefert die ID des Users
+         * @return ID
+         **/
+        function id()
+        {
+            return $this->mcID;
+        }
+
+
+        /** liefert den Studiengang des Users inkl. dem Abschluss
+         * @return Studiengang
+         **/
+        function studiengang()
+        {
+            abschluss ziehen aus: studiengänge, studiengänge_abschluss und user_studiengang ggf user_studiengang_back
+        }
+
+
+        /** liefert die Matrikelnummer des Users, sofern vorhanden
+         * @return Matrikelnummer oder null
+         **/
+        function matrikelnummer()
+        {
+            return MatrikelNummerFactory::get()->get( $this->id );
+        }
+
+
+        /** liefert den vollständigen Namen des Users
+         * @return Name
+         **/
+        function name()
+        {
+
+        }
+
+
+        /** liefert die Fachsemesteranzahl des Users
+         * @return Semesteranzahl
+         **/
+        function semesteranzahl()
+        {
+
+        }
+
+    }
+
+
+
+
+?>
