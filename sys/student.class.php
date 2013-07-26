fa3b4abafd4d22c3b9761f7a62a3b8591ec02200
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-07-26 10:24:49 +0200

    email

--- sys/student.class.php
+++ sys/student.class.php
@@ -59,7 +59,7 @@
             {
                 $lo            = new User($px);
                 echo "<pre>";
-                var_dump(UserModel::geUser($px));
+                var_dump(User::find($px)->email);
                 echo "</pre>";
                 $this->mcName  = $lo->getFullName();
                 //$this->mcEmail =
