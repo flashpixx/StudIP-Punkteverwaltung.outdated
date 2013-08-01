d635ab6979328bf3e52175e897f1bfc65564f4e5
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-08-01 10:23:53 +0200

    übung log

--- sys/veranstaltung/studentuebung.class.php
+++ sys/veranstaltung/studentuebung.class.php
@@ -278,11 +278,16 @@
         {
             $la = array();
 
-            $loPrepare = DBManager::get()->prepare("select erreichtepunkte, zusatzpunkte, bemerkung from ppv_uebungstudentlog where uebung = :uebungid and student = :auth", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY) );
+            $loPrepare = DBManager::get()->prepare("select erreichtepunkte, zusatzpunkte, bemerkung, korrektor from ppv_uebungstudentlog where uebung = :uebungid and student = :auth", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY) );
             $loPrepare->execute( array("uebungid" => $this->moUebung->id(), "auth" => $this->moStudent->id()) );
 
             foreach( $loPrepare->fetchAll(PDO::FETCH_ASSOC) as $row )
+            {
+                $lo               = new User($row["korrektor"]);
+                $row["korrektor"] = $lo->getFullName("full_rev") ." (".User::find($row["korrektor"])->email.")";
+                
                 array_push($la, $row );
+            }
 
             return $la;
         }
