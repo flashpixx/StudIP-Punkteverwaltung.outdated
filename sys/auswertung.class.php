c80938eb7c2167b54aa37ac8bfabf37e5341c710
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-08-03 13:57:42 +0200

    key check

--- sys/auswertung.class.php
+++ sys/auswertung.class.php
@@ -155,9 +155,11 @@
                 {
                     $loStudentUebung = new StudentUebung( $uebung, $key );
                     $main["uebungen"][$lcUebungName]["studenten"][$studentuebung->student()->id()] = $this->createStudentenPunkteArray( $loStudentUebung, $uebungarray["bestandenpunkte"], $uebungarray["maxPunkte"] );
+                    var_dump($loStudentUebung);
                 }
                     
             }
+            die(" ");
 
 
 
