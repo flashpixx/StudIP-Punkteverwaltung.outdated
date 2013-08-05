a8123a01208e9f6ee08bca45815be0d59993dbd8
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-08-05 11:02:13 +0200

    color

--- views/auswertung/index.php
+++ views/auswertung/index.php
@@ -55,22 +55,22 @@
         $i=0;
         foreach ($laListe["studenten"] as $lcStudentKey => $laStudent)
         {
-            echo "<tr style=\"".( $i++ % 2 ? "background=#dddddd" : "background=#aaaaaa")."\">";
-            echo "<td>".$laStudent["name"]." (".$laStudent["email"].")</td>";
-            echo "<td>".$laStudent["matrikelnummer"]."</td>";
+            $lcStyle = $i++ % 2 ? "background=#dddddd" : "background=#aaaaaa";
+
+            echo "<tr>";
+            echo "<td style=\"".$lcStyle."\">".$laStudent["name"]." (".$laStudent["email"].")</td>";
+            echo "<td style=\"".$lcStyle."\">".$laStudent["matrikelnummer"]."</td>";
 
             foreach($laUebungen as $lcUebung)
             {
-                echo "<td>";
+                echo "<td style=\"".$lcStyle."\">";
                 echo $laListe["uebungen"][$lcUebung][$laStudent["id"]]["erreichtepunkte"]." (".($laListe["uebungen"][$lcUebung][$laStudent["id"]]["bestanden"] ? "ja" : "nein").")";
                 echo "</td>";
             }
-            echo "<td>".($laStudent["veranstaltungenbestanden"] ? "ja" : "nein")."</td>";
-            echo "<td>&nbsp;</td>";
+            echo "<td style=\"".$lcStyle."\">".($laStudent["veranstaltungenbestanden"] ? "ja" : "nein")."</td>";
+            echo "<td style=\"".$lcStyle."\">&nbsp;</td>";
             echo "</tr>";
         }
-
-        
 
         echo "</table>";
 
