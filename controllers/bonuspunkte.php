dec34e59c7316c13fc1ea1218a8cc875e3aed715
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-08-06 16:10:40 +0200

    bonuspunkte

---
+++ controllers/bonuspunkte.php
@@ -0,0 +1,70 @@
+<?php
+
+   /**
+    @cond
+    ############################################################################
+    # GPL License                                                              #
+    #                                                                          #
+    # This file is part of the StudIP-Punkteverwaltung.                        #
+    # Copyright (c) 2013, Philipp Kraus, <philipp.kraus@tu-clausthal.de>       #
+    # This program is free software: you can redistribute it and/or modify     #
+    # it under the terms of the GNU General Public License as                  #
+    # published by the Free Software Foundation, either version 3 of the       #
+    # License, or (at your option) any later version.                          #
+    #                                                                          #
+    # This program is distributed in the hope that it will be useful,          #
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+    # GNU General Public License for more details.                             #
+    #                                                                          #
+    # You should have received a copy of the GNU General Public License        #
+    # along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+    ############################################################################
+    @endcond
+    **/
+
+
+
+    /** Controller für die Sicht eines Studenten **/
+    class BonuspunkteController extends StudipController
+    {
+
+        /** Before-Aufruf zum setzen von Defaultvariablen
+         * @warn da der StudIPController keine Session initialisiert, muss die
+         * Eigenschaft "flash" händisch initialisiert werden, damit persistent die Werte
+         * übergeben werden können
+         **/
+        function before_filter( &$action, &$args )
+        {
+            // PageLayout::setTitle("");
+            $this->set_layout($GLOBALS["template_factory"]->open("layouts/base_without_infobox"));
+
+            // Initialisierung der Session & setzen der Veranstaltung, damit jeder View
+            // die aktuellen Daten bekommt
+            $this->flash                  = Trails_Flash::instance();
+            $this->flash["veranstaltung"] = Veranstaltung::get();
+        }
+
+
+        /** Default Action **/
+        function index_action() { }
+
+
+        /** URL Aufruf **/
+        function url_for($to)
+        {
+            $args = func_get_args();
+
+            # find params
+            $params = array();
+            if (is_array(end($args)))
+                $params = array_pop($args);
+
+            # urlencode all but the first argument
+            $args    = array_map("urlencode", $args);
+            $args[0] = $to;
+
+            return PluginEngine::getURL($this->dispatcher->plugin, $params, join("/", $args));
+        }
+        
+    }
