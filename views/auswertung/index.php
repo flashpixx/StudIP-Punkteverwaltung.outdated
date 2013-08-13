768374b17b41b907df8bdea59f7b0baf1426413d
Philipp Kraus <philipp.kraus@flashpixx.de>
2013-08-13 12:29:38 +0200

    layout

--- views/auswertung/index.php
+++ views/auswertung/index.php
@@ -63,7 +63,7 @@ echo "<p><div id=\"boxplot\" style=\"height: 350px; width: 650px; background-col
 */
         echo "<script type=\"text/javascript\">";
 
-        echo "var margin = {top: 10, right: 10, bottom: 10, left: 10}, width = 650 - margin.left - margin.right, height = 350 - margin.top - margin.bottom;";
+        echo "var margin = {top: 50, right: 50, bottom: 50, left: 50}, width = 650 - margin.left - margin.right, height = 350 - margin.top - margin.bottom;";
         echo "var min    = Infinity, max = -Infinity;";
         echo "var chart  = d3.box().whiskers(iqr(1.5)).width(width).height(height);";
 
