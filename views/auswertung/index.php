f85ed53ca4968efb9ced2e4799721853e94f65e1
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-01-20 13:30:36 +0100

    Naming

--- views/auswertung/index.php
+++ views/auswertung/index.php
@@ -48,7 +48,7 @@
         // erzeuge Ausgabe ald PDF
         echo "<h1>Datei Export</h1>";
         echo "<p><table border=\"0\" width=\"35%\">";
-        echo "<tr><td>"._("Liste für Aushang (nur Matrikelnummer, bestanden (ja/nein) und Bonuspunkte)")."</td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "pdf", "extern" => true))."\">PDF</a></td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "xlsx", "extern" => true))."\">XSLX</a></td></tr>";
+        echo "<tr><td>"._("Liste für Aushang (Matrikelnummer, bestanden, Bonuspunkte)")."</td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "pdf", "extern" => true))."\">PDF</a></td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "xlsx", "extern" => true))."\">XSLX</a></td></tr>";
         echo "<tr><td>"._("vollständige Liste")."</td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "pdf"))."\">PDF</a></td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "xlsx"))."\">XSLX</a></td></tr>";
         echo "<tr><td>"._("nur bestandene Studenten")."</td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "pdf", "bestandenonly" => true))."\">PDF</a></td><td><a href=\"".$controller->url_for("auswertung/export", array("type" => "xlsx", "bestandenonly" => true))."\">XSLX</a></td></tr>";
         echo "</table> </p>";
