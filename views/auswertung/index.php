ac74da22b54f01fdeac8de3d93d3c8d8c15da081
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-01-27 14:00:38 +0100

    Statistik & Layout

--- views/auswertung/index.php
+++ views/auswertung/index.php
@@ -127,11 +127,12 @@
 
 
         // hier muss noch etwas die Auswertung hinein
-        echo "<p><table width=\"40%\">";
+        echo "<p><table width=\"25%\">";
         echo "<tr><th>Teilnehmeranzahl</th><td>".$laListe["statistik"]["teilnehmergesamt"]."</td></tr>";
         echo "<tr><th>Anzahl bestandene Studenten (%)</th><td>".$laListe["statistik"]["teilnehmerbestanden"]." (".round($laListe["statistik"]["teilnehmerbestanden"] / $laListe["statistik"]["teilnehmergesamt"] * 100, 2)."%)</td></tr>";
         echo "<tr><th>Anzahl Studenten mit Bonuspunkten (% der bestanden)</th><td>".$laListe["statistik"]["teilnehmerbonus"]." (".round($laListe["statistik"]["teilnehmerbonus"] / $laListe["statistik"]["teilnehmerbestanden"],2)."%)</td></tr>";
-        echo "<tr><th>Gesamtpunktanzahl</th><td>".$laListe["gesamtpunktebestanden"]."</td></tr>";
+        echo "<tr><th>Gesamtpunktanzahl</th><td>".$laListe["gesamtpunkte"]."</td></tr>";
+        echo "<tr><th>Punkte zum Bestehen</th><td>".$laListe["gesamtpunktebestanden"]."</td></tr>";
         echo "<tr><th>max. erreichte Punkte</th><td>".$laListe["statistik"]["maxpunkte"]."</td></tr>";
         echo "<tr><th>min. erreichte Punkte</th><td>".$laListe["statistik"]["minpunkte"]."</td></tr>";
         echo "</table></p>";
