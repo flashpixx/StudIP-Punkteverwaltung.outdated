acdb5cf0e467f213153a024f722e5bc9a3b94526
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-11-28 09:28:22 +0100

    Logo

--- views/admin/index.php
+++ views/admin/index.php
@@ -81,7 +81,8 @@
                 echo "<p><input type=\"submit\" name=\"submitted\" value=\""._("Angaben übernehmen")."\"/></p>";
 
             echo "</form></p><br/><br/>";
-            echo "<p class=\"ppv_ci\"><a target=\"_blank\" href=\"https://github.com/flashpixx/StudIP-Punkteverwaltung\">https://github.com/flashpixx/StudIP-Punkteverwaltung</a></p>";
+            echo "<p><a href=\"https://github.com/flashpixx/StudIP-Punkteverwaltung\"><img style=\"position: absolute; top: 0; left: 0; border: 0;\" src=\"https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67\" alt=\"Fork me on GitHub\" data-canonical-src=\"https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png\"></a></p>";
+            //echo "<p class=\"ppv:ci\"><a target=\"_blank\" href=\"https://github.com/flashpixx/StudIP-Punkteverwaltung\">https://github.com/flashpixx/StudIP-Punkteverwaltung</a></p>";
         }
         
     } catch (Exception $e) {
