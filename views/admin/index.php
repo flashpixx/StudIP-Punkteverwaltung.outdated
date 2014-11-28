5b63fa47f3a05e30a9411cfc0433b1d4cf4c0223
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-11-28 10:27:01 +0100

    Move CSS class

--- views/admin/index.php
+++ views/admin/index.php
@@ -82,7 +82,7 @@
 
             echo "</form></p><br/><br/>";
 
-            echo "<p><a target=\"_blank\" href=\"https://github.com/flashpixx/StudIP-Punkteverwaltung\" class=\"ppv:ci\"><img src=\"".$plugin->getPluginURL()."/assets/github.png"."\" /> https://github.com/flashpixx/StudIP-Punkteverwaltung</a></p>";
+            echo "<p class=\"ppv:ci\"><a target=\"_blank\" href=\"https://github.com/flashpixx/StudIP-Punkteverwaltung\"><img src=\"".$plugin->getPluginURL()."/assets/github.png"."\" /> https://github.com/flashpixx/StudIP-Punkteverwaltung</a></p>";
         }
         
     } catch (Exception $e) {
