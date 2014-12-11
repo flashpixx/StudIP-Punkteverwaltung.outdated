be8f1dd8920326695ca63a3986cbcfae17782d06
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-12-11 19:31:35 +0100

    Column add

--- views/auswertung/index.php
+++ views/auswertung/index.php
@@ -168,12 +168,13 @@
         echo "fields: {";
         
         echo "Auth : { key : true, create : false, edit : false, list : false },";
+        echo "Hinweis : { edit : false, title : \"Hinweis\", width : \"10%\" },";
         echo "Matrikelnummer : { edit : false, title : \"Matrikelnummer\", width : \"10%\" },";
         echo "Name : { edit : false, title : \"Name\", width : \"10%\" },";
         echo "EmailAdresse : { edit : false, title : \"EMail Adresse\", width : \"10%\" },";
         echo "Studiengang : { edit : false, title : \"Studiengang\", width : \"10%\" },";
         
-        $lnSize = round( 60 / count($laListe["uebungen"]),1);
+        $lnSize = round( 50 / count($laListe["uebungen"]),1);
         foreach($laListe["uebungen"] as $laUebung)
             echo "ueb".md5($laUebung["name"])." : { edit : false, title : \"".$laUebung["name"]."  ("._("bestanden").")\", width : \"".$lnSize."%\" },";
         
