8c7ac7c973be5defa020025ebf49fd8fc2158825
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-12-16 22:35:21 +0100

    Gruppencall

--- controllers/uebung.php
+++ controllers/uebung.php
@@ -252,12 +252,12 @@
                     foreach( $laData as $item )
                     {
                         // siehe Arraykeys unter views/uebung/jsonlist.php & alle String müssen UTF-8 codiert werden, da Json UTF-8 ist
-                        $lxGruppen = array();
-                        foreach( $item->student()->gruppen($this->flash["veranstaltung"]) as $item )
-                            array_push($lxGruppen, $item["name"]);
+                        //$lxGruppen = array();
+                        foreach( $item->student()->gruppen($this->flash["veranstaltung"]) as $item ) {}
+                            //array_push($lxGruppen, $item["name"]);
                         //$lxGruppen = empty($lxGruppen) ? null : implode(", ", $lxGruppen);
-                        $lxGruppen = print_r($lxGruppen, true);
-                        
+                        //$lxGruppen = print_r($lxGruppen, true);
+                        $lxGruppen = "";
                         
                         $laItem = array(
                                     "Auth"            => studip_utf8encode( $item->student()->id() ),
