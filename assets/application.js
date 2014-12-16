78ce7a32c2d17d546cd062379008c9ad0c46bc65
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-12-16 21:18:17 +0100

    add Contextmenu

--- assets/application.js
+++ assets/application.js
@@ -6,6 +6,22 @@
              window.location = $(":selected",this).attr("rel")
         });
                            
+                           
+       $("#punktetabelle").contextmenu({
+             delegate: ".hasmenu",
+             menu: [
+                    {title: "Copy", cmd: "copy", uiIcon: "ui-icon-copy"},
+                    {title: "----"},
+                    {title: "More", children: [
+                    {title: "Sub 1", cmd: "sub1"},
+                    {title: "Sub 2", cmd: "sub1"}
+             ]}
+                                                              ],
+             select: function(event, ui) {
+                    alert("select " + ui.cmd + " on " + ui.target.text());
+             }
+       });
+                           
     });
 
 }(jQuery));
