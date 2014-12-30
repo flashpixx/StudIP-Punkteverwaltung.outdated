80e958b7a028cb7c3c501e7c61a61c15c1f73849
Philipp Kraus <philipp.kraus@flashpixx.de>
2014-12-30 23:39:28 +0100

    Array Struktur

--- controllers/show.php
+++ controllers/show.php
@@ -158,8 +158,8 @@
                 foreach( $this->auswertung["uebungen"] as $laUebung )
                     array_push( $la, $this->createPunkteTableRow($laUebung["name"], $laUebung["studenten"][$this->student->id()]["punktesumme"], $laUebung["studenten"][$this->student->id()]["erreichteprozent"]), $laUebung["studenten"][$this->student->id()]["score"] );
 
-                $this->createPunkteTableRow();
-                $this->createPunkteTableRow(_("Bonuspunkte"), $this->auswertung["uebungen"]["studenten"][$this->student->id()]["bonuspunkte"]);
+                array_push( $la, $this->createPunkteTableRow() );
+                array_push( $la, $this->createPunkteTableRow(_("Bonuspunkte"), $this->auswertung["uebungen"]["studenten"][$this->student->id()]["bonuspunkte"]) );
                 
 
                 // alles fehlerfrei durchlaufen, setze Result
